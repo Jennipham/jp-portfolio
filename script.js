97923442e361e79d69caddf5f02873f57809b9e1
Jennipham <30297540+Jennipham@users.noreply.github.com>
2024-12-12 21:14:04 +0000

    feat: adding flipcard for betterment

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function flipCard(button) {
+    const card = button.closest('.flip-card');
+    card.classList.toggle('flipped');
+  }
+  
